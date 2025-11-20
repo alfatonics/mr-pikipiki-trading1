dcd2c045ecbfefd2a8b9e9eb2e8cb57134e8bbae
alfatonics <alfatonicstz@gmail.com>
2025-11-20 13:03:30 +0300

    Add detailed logging in api handler to debug 405

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 import app from "../server/app.js";
 
 export default function handler(req, res) {
+  const { method, url } = req;
+  const contentType = req.headers["content-type"];
+
+  console.log("🔔 API entry:", {
+    method,
+    url,
+    contentType,
+    contentLength: req.headers["content-length"],
+  });
+
+  res.on("finish", () => {
+    console.log("✅ API response:", {
+      method,
+      url,
+      statusCode: res.statusCode,
+    });
+  });
+
   return app(req, res);
 }
